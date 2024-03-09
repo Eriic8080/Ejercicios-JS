46976205294aa57040b596bb84c22824b4258674
= <=>
2024-03-09 16:23:59 +0100

    JS Terminado de la actividad GEOCODE

--- UF3/Google Geocode/js/script.js
+++ UF3/Google Geocode/js/script.js
@@ -1,4 +1,5 @@
 let map;
+let address = "";
 
 async function initMap() {
     //Aqui importo la biblioteca de google maps
@@ -14,24 +15,71 @@ async function initMap() {
   new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "Hello World!",
+    title: "ITICBCN",
   });
+  document.getElementById("adreca").value = "Carrer de la Selva de Mar 211 08020 Barcelona";
 }
   window.initMap = initMap;
 
-async function geoCode(){
+async function geoCode() {
   let geocoder = new google.maps.Geocoder();
-  let address="Carrer de la Selva de Mar 211 08020 Barcelona";
-  geocoder.geocode( { 'address': address}, function(results, status) {
+  geocoder.geocode({ 'address': address }, function (results, status) {
   
-    if (status == google.maps.GeocoderStatus.OK) {
+  if (status == google.maps.GeocoderStatus.OK) {
     latitude = results[0].geometry.location.lat();
-    console.log(latitude);
     longitude = results[0].geometry.location.lng();
-    console.log(longitude);
+  
+    let center = new google.maps.LatLng(latitude, longitude);
+    map.setCenter(center);
+    map.setZoom(16);
+  
+  
+    new google.maps.Marker({
+      position: center,
+      map,
+      title: address,
+      });
+    } else {
+      console.error("ERROR: " + status);
     }
   });
 }
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  //Aqui miro si se pulsa el boton de buscar, me guardo la informacion del input para depues ir a la funcion geoCode
+  //y buscar la informacion por la API de GOOGLE MAPS
+  document.getElementById("findLoc").addEventListener("click", function () {
+    address = document.getElementById("adreca").value;
+    geoCode();
+  });
 
+  //Aqui escucho el boton de mi ubicación para buscar mi ubicacion por la API de GOOGLE MAPS en la funcion geoCodeMe
+  document.getElementById("findMe").addEventListener("click", function () {
+    geoCodeMe();
+  });
+});
+
+async function geoCodeMe() {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+      map.setZoom(9);
+      let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: "Mi UBICAIÓN",
+        icon: {
+          url: 'img/marker.png',
+          scaledSize: new google.maps.Size(32, 32)
+        }
+      });
+    });
+  }
+
+}
